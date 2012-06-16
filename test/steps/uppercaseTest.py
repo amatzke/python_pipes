6514f58ceba66c18b2cdfdc4756edeadc98e1656
amatzke <amy5rose@gmail.com>
2012-06-16 19:22:41 -0400

    Adding basic unit test for the uppercase class

---
+++ test/steps/uppercaseTest.py
@@ -0,0 +1,38 @@
+import unittest
+
+from pipes.steps.uppercase import Step
+
+class uppercaseTest(unittest.TestCase):
+    """
+    A test class for the uppercaseTest module.
+    """
+    
+    def setUp(self):
+        """
+        set up data used in the tests.
+        setUp is called before each test function execution.
+        """
+        self.originalKey = 'original'
+        self.originalValue = 'Hello Python'
+        self.inputList = {self.originalKey: self.originalValue}
+        
+
+
+    def runTest(self):
+        """
+        Test a successful run. 
+        1.The original value should not have changed. 
+        2.The upper case value should be correct  
+        """
+        step = Step()
+        step.run(self.inputList)
+
+        
+        self.assertEqual(self.originalValue, self.inputList[self.originalKey])
+        self.assertEqual(self.originalValue.upper(), self.inputList['UCASE'])
+        
+        
+        self.assertEqual(self.originalValue, self.inputList['UCASE'])
+        
+
+        
